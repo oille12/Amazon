81caa1e0de8f4cad438afa41b316b4b6a4f50677
oille12 <oeg9176@naver.com>
2024-04-01 20:18:01 +0900

    240401 Amazon

--- src/component/ProductImg.js
+++ src/component/ProductImg.js
@@ -4,8 +4,9 @@ import { useState } from "react";
 const PHOTOS = ["/images/product-1.jpeg","/images/product-2.jpeg","/images/product-3.jpeg"];
 
 export default function ProductImg() {
-
     const [index, setIndex] = useState(0);
+    const firstIndex = 0;
+    const lastIndex = PHOTOS.length-1;
 
     return(
         <>
@@ -16,15 +17,43 @@ export default function ProductImg() {
                 src={PHOTOS[index]}
                 alt={PHOTOS[index]}
             />
-            {/* 좌우 화살표 */}
+
+            {/* left 화살표 */}
+            {index !== firstIndex && (
+                <button
+                    className="absolute top-0 left-0 h-full px-2 text-4xl text-gray-400"
+                    onClick={() => setIndex(index-1)}
+                >
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/>
+                    </svg> */}
+                    &#10094;
+                </button>
+            )}
+
+            {/* right 화살표 */}
+            {index !== lastIndex && (
+                <button
+                    className="absolute top-0 right-0 h-full px-2 text-4xl text-gray-400"
+                    onClick={() => setIndex(index+1)}
+                >
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                    </svg> */}
+                    &#10095;
+                </button>
+            )}
         </div>
 
-        {/* 점 3개 */}
+        {/* 점 3개 동적 구현 - map활용 */}
         <div className="flex justify-center my-4">
             {PHOTOS.map((photo, photoIndex) => (
                 <span
-                    className="w-2 h-2 mx-1 rounded-full bg-gray-200"
-                ></span>
+                    key={photoIndex}
+                    className="w-2 h-2 mx-1 rounded-full"
+                    style={{backgroundColor: (index === photoIndex) ? "orange" : "lightgray"}}
+                >
+                </span>
             ))}
         </div>
 
